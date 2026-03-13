8457c5197caccea7142fbff07e5cf40462c29632
ProfeHectaime1 <profe.hectaime@gmail.com>
2026-03-13 15:55:57 +0100

    scrollbar updated on all the web

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -47,15 +47,6 @@ import {
 
 
 const Home = ({ user }) => {
-    // Add .home-scrollbar-active to html/body on mount, remove on unmount
-    React.useEffect(() => {
-        document.documentElement.classList.add('home-scrollbar-active');
-        document.body.classList.add('home-scrollbar-active');
-        return () => {
-            document.documentElement.classList.remove('home-scrollbar-active');
-            document.body.classList.remove('home-scrollbar-active');
-        };
-    }, []);
 
     const [searchQuery, setSearchQuery] = useState('');
     const [sharedScopeSelected, setSharedScopeSelected] = useState(true);
